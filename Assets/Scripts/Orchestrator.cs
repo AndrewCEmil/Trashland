d8284e5ab3023c09ece07328471ef3d23e217a5e
ace <andrew.emil@gmail.com>
2018-07-09 18:39:23 -0700

    Initial commit

---
+++ Assets/Scripts/Orchestrator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orchestrator : MonoBehaviour {
+
+    GameObject startBall;
+    Rigidbody startBallRb;
+	// Use this for initialization
+	void Start () {
+        startBall = GameObject.Find("StartBall");
+        startBallRb = startBall.GetComponent<Rigidbody>();
+	}
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.anyKeyDown) {
+            startBallRb.useGravity = true;
+        }
+	}
+}
