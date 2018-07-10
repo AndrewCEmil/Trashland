5b046617daa32aae3c5c419b1fc8a8707e45ce1e
ace <andrew.emil@gmail.com>
2018-07-09 19:36:11 -0700

    Added sample mirror

---
+++ Assets/Scripts/MirrorController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MirrorController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("TRIGGED");
+        if (other.name == "Floor")
+        {
+            return;
+        }
+
+        Rigidbody otherRb = other.GetComponent<Rigidbody>();
+        otherRb.velocity = otherRb.velocity * -1f;
+    }
+}
